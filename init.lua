126e601e22ff807fbf2904e827b28003925ea5d9
unsecretised <no1umang@gmail.com>
2025-12-24 18:41:12 +0800

    more things

--- init.lua
+++ init.lua
@@ -32,6 +32,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
       end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rust", "rs", "Rust" },
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gleam", "html", "css", "javascript", "typescript", "typescriptreact", "javascriptreact" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
 -- vim.api.nvim_create_autocmd("LspAttach", {
 -- 	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
 -- 	callback = function(args)
